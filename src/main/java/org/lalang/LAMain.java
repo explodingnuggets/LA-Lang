234b39f7dc5f2a0b3a2e204a1f35efa9afe06aa7
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2018-08-12 21:06:34 -0300

    Criada a classe main para o ponto de entrada do compilador

---
+++ src/main/java/org/lalang/LAMain.java
@@ -0,0 +1,25 @@
+package org.lalang;
+
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+class LAMain {
+    public static void main(String[] args){
+        if(args.length < 1) {
+            System.out.println("java -jar la source_code");
+        } else {
+            try {
+                CharStream input = CharStreams.fromFileName(args[0]);
+                LALexer lexer = new LALexer(input);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                LAParser parser = new LAParser(tokens);
+
+            } catch (IOException exception) {
+                System.out.println("Erro ao abrir o arquivo: " + exception.getMessage());
+            }
+        }
+    }
+}
