8c66378ab24b9d36c97e8e5abfd0dcadbd368c14
Rene Ferrante <rene.ferranten@gmail.com>
2018-09-17 02:00:58 -0300

    DeclVariavel gerador de codigo

--- src/main/java/org/lalang/LAGeracao.java
+++ src/main/java/org/lalang/LAGeracao.java
@@ -7,6 +7,22 @@ class LAGeracao extends LABaseListener {
         this.out = out;
     }
 
+    public String getTipo(String tipo) {
+        if(tipo=="inteiro"){
+            return "int";
+        }
+        else if(tipo=="literal"){
+            return "string";
+        }
+        else if(tipo=="real"){
+            return "float";
+        }
+        else if(tipo=="logico"){
+            return "int";
+        }
+        return null;
+    }
+
     @Override
     public void enterPrograma(LAParser.ProgramaContext ctx) {
         this.out.append("#include <stdio.h>\n");
@@ -15,11 +31,22 @@ class LAGeracao extends LABaseListener {
 
     @Override
     public void exitPrograma(LAParser.ProgramaContext ctx) {
-        this.out.append("}");
+        this.out.append("return 0;\n}");
     }
 
     @Override
     public void exitDeclaracoes(LAParser.DeclaracoesContext ctx) {
         this.out.append("int main() {\n");
+    }
+
+    @Override
+    public void enterDeclVariavel(LAParser.DeclVariavelContext ctx) {
+        this.out.append(getTipo(ctx.variavel().tipo().getText())+ " " + ctx.variavel().first.getText()+";\n");
+    }
+
+    
+    @Override 
+    public void enterCmdLeia(LAParser.CmdLeiaContext ctx) {
+
     }
 }
