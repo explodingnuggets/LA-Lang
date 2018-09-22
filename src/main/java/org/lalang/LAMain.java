4ffaf19a5f79d4315786426483f68141f6f8cf3d
Rene Ferrante <rene.ferranten@gmail.com>
2018-09-22 15:58:50 -0300

    documentado LAMain

--- src/main/java/org/lalang/LAMain.java
+++ src/main/java/org/lalang/LAMain.java
@@ -14,6 +14,7 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 class LAMain {
     public static void main(String[] args){
+        //se recebeu o número de argumentos errado mostra como eles devem ser inseridos
         if(args.length < 1) {
             System.out.println("java -jar la source_code [output_file]");
         } else {
@@ -41,16 +42,19 @@ class LAMain {
                 parser.addErrorListener(error_listener);
                 LAParser.ProgramaContext ast = parser.programa();
                 
+                //caso não houveram erros sintáticos verificar se houveram erros semânticos
                 if(!error_out.modified()) {
                     LASemantico semantico = new LASemantico(error_out);
                     semantico.visitPrograma(ast);
                 }
 
+                //caso não ouveram erros semânticos também gerar o código em C
                 if(!error_out.modified()) {
                     LAGeracao geracao = new LAGeracao(out);
                     geracao.visitPrograma(ast);
                 }
 
+                // Se não foram passados dois argumentos, a saída será escrita no stdout (terminal)
                 // Se foram passados dois argumentos, o segundo será tratado como
                 // o caminho de um arquivo, onde a saída será escrita
                 if(args.length < 2) {
@@ -72,6 +76,7 @@ class LAMain {
                     file_out.flush();
                     file_out.close();
                 }
+            //caso haja uma excessão ao lidar com o arquivo imprimir mensagem correspondente
             } catch (IOException exception) {
                 System.out.println("Erro ao abrir o arquivo: " + exception.getMessage());
             }
